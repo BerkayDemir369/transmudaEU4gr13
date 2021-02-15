758372ab3e518b87d91e42f2d7af07ae88f453be
BerkayDemir369 <berkay.demir369@gmail.com>
2021-02-16 00:03:47 +0200

    DriverClass changed

--- src/test/java/com/transmuda/utilities/Driver.java
+++ src/test/java/com/transmuda/utilities/Driver.java
@@ -6,69 +6,124 @@ import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class Driver {
-
     private Driver() {
+
     }
-    // InheritableThreadLocal  --> this is like a container, bag, pool.
-    // in this pool we can have separate objects for each thread
-    // for each thread, in InheritableThreadLocal we can have separate object for that thread
-    // driver class will provide separate webdriver object per thread
-    private static InheritableThreadLocal<WebDriver> driverPool = new InheritableThreadLocal<>();
+
+    private static WebDriver driver;
+
     public static WebDriver get() {
-        //if this thread doesn't have driver - create it and add to pool
-        if (driverPool.get() == null) {
-//            if we pass the driver from terminal then use that one
-//           if we do not pass the driver from terminal then use the one properties file
-            String browser = System.getProperty("browser") != null ? browser = System.getProperty("browser") : ConfigurationReader.get("browser");
+
+        // Test
+        if (driver == null) {
+            // this line will tell which browser should open based on the value from properties file
+            String browser = ConfigurationReader.get("browser");
             switch (browser) {
                 case "chrome":
                     WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver());
+                    driver = new ChromeDriver();
                     break;
+
+                case "chromeAcceptSslError":
+                    ChromeOptions handlingSSL = new ChromeOptions();
+                    //Using the accept insecure cert method with true as parameter to accept the untrusted certificate
+                    handlingSSL.setAcceptInsecureCerts(true);
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver(handlingSSL);
+                    break;
+
                 case "chrome-headless":
                     WebDriverManager.chromedriver().setup();
-                    driverPool.set(new ChromeDriver(new ChromeOptions().setHeadless(true)));
+                    driver = new ChromeDriver(new ChromeOptions().setHeadless(true));
                     break;
+
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver());
+                    driver = new FirefoxDriver();
                     break;
+
+                case "firefoxAcceptSslError":
+                    //Creating an object of the FirefoxOptions Class
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    //Using the setAcceptInsecureCerts() method to pass parameter as False
+                    firefoxOptions.setAcceptInsecureCerts(false);
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver(firefoxOptions);
+                    break;
+
                 case "firefox-headless":
                     WebDriverManager.firefoxdriver().setup();
-                    driverPool.set(new FirefoxDriver(new FirefoxOptions().setHeadless(true)));
+                    driver = new FirefoxDriver(new FirefoxOptions().setHeadless(true));
                     break;
+
                 case "ie":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Internet Explorer");
                     WebDriverManager.iedriver().setup();
-                    driverPool.set(new InternetExplorerDriver());
+                    driver = new InternetExplorerDriver();
                     break;
+
                 case "edge":
                     if (!System.getProperty("os.name").toLowerCase().contains("windows"))
                         throw new WebDriverException("Your OS doesn't support Edge");
                     WebDriverManager.edgedriver().setup();
-                    driverPool.set(new EdgeDriver());
+                    driver = new EdgeDriver();
                     break;
+
+                case "edgeAcceptSslError":
+                    //Creating an object of EdgeOptions class
+                    EdgeOptions edgeOptions = new EdgeOptions();
+
+                    //Accepting the Insecure certificates through boolean parameter
+                    edgeOptions.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+                    if (!System.getProperty("os.name").toLowerCase().contains("windows"))
+                        throw new WebDriverException("Your OS doesn't support Edge");
+                    WebDriverManager.edgedriver().setup();
+                    driver = new EdgeDriver(edgeOptions);
+                    break;
+
+
+                //                driver.get("https://zerobank......");
+                //
+                //                JavascriptExecutor js = (JavascriptExecutor)driver;
+                //                js.executeScript("CertificateWarningController.visitInsecureWebsiteWithTemporaryBypass()");
+
+
                 case "safari":
                     if (!System.getProperty("os.name").toLowerCase().contains("mac"))
                         throw new WebDriverException("Your OS doesn't support Safari");
                     WebDriverManager.getInstance(SafariDriver.class).setup();
-                    driverPool.set(new SafariDriver());
+                    driver = new SafariDriver();
                     break;
+
+
+
+//                driver.get("https://zerobank......");
+//
+//                JavascriptExecutor js = (JavascriptExecutor)driver;
+//                js.executeScript("CertificateWarningController.visitInsecureWebsiteWithTemporaryBypass()");
+
+
+
             }
+
         }
-        return driverPool.get();
+
+        return driver;
     }
+
     public static void closeDriver() {
-        driverPool.get().quit();
-        driverPool.remove();
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
 }
-
-
