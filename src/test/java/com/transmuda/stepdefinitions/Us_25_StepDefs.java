3c5f204e17b1204b8b3a453e4ead84fff64c4798
Hamdulla.A <hamdulla3@gmail.com>
2021-02-25 17:59:58 +0100

    Hamdulla's Class updated

--- src/test/java/com/transmuda/stepdefinitions/Us_25_StepDefs.java
+++ src/test/java/com/transmuda/stepdefinitions/Us_25_StepDefs.java
@@ -1,6 +1,8 @@
 package com.transmuda.stepdefinitions;
 
+import com.transmuda.pages.DashboardPage;
 import com.transmuda.pages.VehicleCostsPage;
+import com.transmuda.utilities.BrowserUtils;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,44 +11,46 @@ public class Us_25_StepDefs {
     VehicleCostsPage vehicleCostsPage=new VehicleCostsPage();
     @Given("navigate to Fleet Vehicle Costs")
     public void navigate_to_Fleet_Vehicle_Costs() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        DashboardPage dashboardPage=new DashboardPage();
+        dashboardPage.navigateToModule("Fleet","Vehicle Costs");
+        BrowserUtils.waitFor(5);
     }
 
     @When("click on the Page button on the Vehicle Costs page")
     public void click_on_the_Page_button_on_the_Vehicle_Costs_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       vehicleCostsPage.pageChangeButton.click();
+       BrowserUtils.waitFor(3);
     }
 
     @Then("truck driver can change page number")
     public void truck_driver_can_change_page_number() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       vehicleCostsPage.viewPerPageChooseButton.click();
+
     }
 
     @When("click on the View Per Page button on the Vehicle Costs page")
     public void click_on_the_View_Per_Page_button_on_the_Vehicle_Costs_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        vehicleCostsPage.viewPerPageChooseButton.click();
     }
 
     @Then("truck driver can change the entity number")
     public void truck_driver_can_change_the_entity_number() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       vehicleCostsPage.perPage10Button.click();
+       vehicleCostsPage.perPage25Button.click();
+       vehicleCostsPage.perPage50Button.click();
+       vehicleCostsPage.perPage100Button.click();
     }
 
     @When("click on the Export Grid button on the Vehicle Costs page")
     public void click_on_the_Export_Grid_button_on_the_Vehicle_Costs_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        vehicleCostsPage.exportGridButton.click();
+        vehicleCostsPage.csvButton.click();
+        vehicleCostsPage.xlsxButton.click();
     }
 
     @Then("truck driver can get all informations to own email adress")
     public void truck_driver_can_get_all_informations_to_own_email_adress() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
