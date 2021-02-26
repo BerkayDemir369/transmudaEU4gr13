44b11c2aeb7c16862583a978fe76ebe0b3e3bd28
aynurrishat927 <aynurrishat@gmail.com>
2021-02-26 20:28:41 +0100

    new updated aynurupdate4

--- src/test/java/com/transmuda/runners/CukesRunner.java
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/transmuda/stepdefinitions",
         dryRun = false,
-   tags = ""
+   tags = "@TR-171"
 
 )
 public class CukesRunner {
