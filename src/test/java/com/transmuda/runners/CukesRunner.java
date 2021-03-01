810919b758dbd16c0134422b3fba218f5703a956
BerkayDemir369 <berkay.demir369@gmail.com>
2021-03-01 22:59:51 +0200

    Sprint-4 , Us-33 Us-34 features and stepDef are created

--- src/test/java/com/transmuda/runners/CukesRunner.java
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -11,8 +11,8 @@ import org.junit.runner.RunWith;
                 "rerun:target/rerun.txt"},
         features = "src/test/resources/features",
         glue = "com/transmuda/stepdefinitions",
-        dryRun = false,
-        tags = "@TR-205"
+        dryRun = true,
+        tags = "@TR-226"
 )
 public class CukesRunner {
 }
---
+++ src/test/java/com/transmuda/stepdefinitions/Us_33_StepDefs.java
@@ -0,0 +1,72 @@
+package com.transmuda.stepdefinitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Us_33_StepDefs {
+
+
+    @Then("the user should be able to see costs information")
+    public void the_user_should_be_able_to_see_costs_information() {
+
+
+
+
+    }
+
+    @When("the user click Page button to next page or previous page")
+    public void the_user_click_Page_button_to_next_page_or_previous_page() {
+
+
+
+    }
+
+    @Then("the user should be able to see next or previous page")
+    public void the_user_should_be_able_to_see_next_or_previous_page() {
+
+
+
+    }
+
+    @Then("the user click View Per Page button")
+    public void the_user_click_View_Per_Page_button() {
+
+
+
+    }
+
+    @Then("the user should be able to see entity number of View Per page")
+    public void the_user_should_be_able_to_see_entity_number_of_View_Per_page() {
+
+
+
+    }
+
+    @When("the user click any entity number in the View Per Page drop List")
+    public void the_user_click_any_entity_number_in_the_View_Per_Page_drop_List() {
+
+
+
+    }
+
+    @Then("System should be change the entity number of View Per Page")
+    public void system_should_be_change_the_entity_number_of_View_Per_Page() {
+
+
+
+    }
+
+    @Then("the user click Export Grid button")
+    public void the_user_click_Export_Grid_button() {
+
+
+
+    }
+
+    @When("the user select any options of Export Grid Button")
+    public void the_user_select_any_options_of_Export_Grid_Button() {
+
+
+
+    }
+}
