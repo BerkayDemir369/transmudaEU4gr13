e79f3742bb947d826d0bab3ce15d6d5a99210b6b
bahagit <baha@cybertekschool.com>
2021-03-06 19:45:04 +0000

    baha initial push POM fixed /@Smoke/Login.feature changed

--- src/test/java/com/transmuda/runners/CukesRunner.java
+++ src/test/java/com/transmuda/runners/CukesRunner.java
@@ -9,10 +9,12 @@ import org.junit.runner.RunWith;
         plugin = {"json:target/cucumber.json",
                 "html:target/default-html-reports",
                 "rerun:target/rerun.txt"},
+        //monochrome = true, //display the console output in a proper readable format
+        strict = true, //it will check if any step is not defined in step definition file
         features = "src/test/resources/features",
         glue = "com/transmuda/stepdefinitions",
         dryRun = false,
-        tags = "@TR-224"
+        tags = "@smoke"
 )
 public class CukesRunner {
 }
